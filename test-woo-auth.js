a84e7751b55a9c9baac02811daf4eaf1a0954e56
imanmarkchannel1-prog <imanmarkchannel1@gmail.com>
2026-07-02 19:35:29 +0530

    fix: resolve empty product images by mapping sku keywords to media uploads folder, filter by status publish

--- test-woo-auth.js
+++ test-woo-auth.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
+const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
 
 // Parse .env manually
 const envRaw = fs.readFileSync('.env', 'utf-8');
@@ -10,22 +11,24 @@ const consumerKey = env.WOOCOMMERCE_CONSUMER_KEY;
 const consumerSecret = env.WOOCOMMERCE_CONSUMER_SECRET;
 
 const cleanBaseUrl = apiUrl.replace(/\/wp-json\/wc\/v3\/?$/, '').replace(/\/$/, '');
-const targetUrl = `${cleanBaseUrl}/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}&per_page=1`;
 
-console.log(`Testing URL: ${targetUrl}`);
+const WooCommerce = new WooCommerceRestApi({
+  url: cleanBaseUrl,
+  consumerKey: consumerKey,
+  consumerSecret: consumerSecret,
+  version: 'wc/v3'
+});
 
-fetch(targetUrl)
+console.log(`Testing connection using WooCommerce SDK targeting: ${cleanBaseUrl}`);
+
+WooCommerce.get('products', { per_page: 1 })
   .then(res => {
     console.log(`Status: ${res.status} ${res.statusText}`);
-    return res.json();
-  })
-  .then(data => {
-    if (data.code && data.message) {
-      console.log('API Error:', data);
-    } else {
-      console.log(`Success! Retrieved ${data.length} product(s). First product: ${data[0] ? data[0].name : 'None'}`);
-    }
+    const data = res.data;
+    console.log(`Success! Retrieved ${data.length} product(s). First product: ${data[0] ? data[0].name : 'None'}`);
   })
   .catch(err => {
-    console.error('Fetch error:', err);
+    const errorData = err.response ? err.response.data : { message: err.message };
+    const status = err.response ? err.response.status : 500;
+    console.error(`Error status ${status}:`, errorData);
   });
